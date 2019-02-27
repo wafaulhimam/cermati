4c92bfd94d2b2d9037999ba22b3ee5a4de16d395
Muhamammad Wafaul Himam <wafaul.himam@gmail.com>
2019-02-27 11:37:29 +0700

    Edit newsletter and another css

--- news2.js
+++ news2.js
@@ -2,7 +2,23 @@ var w = $(window), d = $(document);
 w.scroll(function() {
    if(w.scrollTop() + w.height() == d.height())
    {
-      console.log("You have hit the bottom!");
-      document.getElementById("myImg").className = "slideUp";
+      console.log("Bottom of the page");
+      document.getElementById("newsletter").className = "slideUp";
    } 
+});
+
+// ===== Scroll to Top ==== 
+var w = $(window), d = $(document);
+w.scroll(function() {
+   if(w.scrollTop() + w.height() == d.height())
+ {        // If page is scrolled more than 50px
+       $('#return-to-top').fadeIn(200);    // Fade in the arrow
+   } else {
+       $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+   }
+});
+$('#return-to-top').click(function() {      // When arrow is clicked
+   $('body,html').animate({
+       scrollTop : 0                       // Scroll to top of body
+   }, 500);
 });
