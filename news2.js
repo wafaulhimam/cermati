ca561f4369a0f4cf9e169a40775a9589e38a65df
Muhamammad Wafaul Himam <wafaul.himam@gmail.com>
2019-03-01 01:53:10 +0700

    Localstorage and setinterval of Newsletter Panel

--- news2.js
+++ news2.js
@@ -15,4 +15,34 @@ $(document).ready(function(){
  });
 //  End Closing Newsletter
 
+// Start Localstorage and Setinterval 10 minutes
+var notif = document.getElementById('newsletter');
+var notifHidden = false;
+var intervalDate;
+
+checkDate = () => {
+  var timeNow = new Date();
+  var timeNowMinutes = timeNow.getMinutes();
+  var timePast = localStorage.getItem('date');
+  var diff = timeNowMinutes - timePast;
+  console.log(diff)
+  if(notifHidden && diff >= 1 ){
+     console.log('ea')
+     notif.style.display = "block";
+     notifHidden = !notifHidden;
+     clearInterval(intervalDate);
+  }
+}
+
+notif.onclick = () => {
+  var time = new Date();
+  notif.style.display = "none";
+  localStorage.setItem('date', time.getMinutes())
+  notifHidden = !notifHidden;
+  intervalDate = setInterval(() => {
+    checkDate();
+  }, 1000)
+}
+
+// End Localstorage and Setinterval 10 minutes
 
